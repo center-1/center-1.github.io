4048a6d227de1ec836ffa29caf2ef5b3b664e634
center-1 <center88@mail.ru>
2019-04-01 18:08:00 +0300

    анимация feedback modal

--- portfolio/gllacy/js/script.js
+++ portfolio/gllacy/js/script.js
@@ -1,14 +1,23 @@
 var contacts_button = document.querySelector(".contacts-button");
 var modal_feedback = document.querySelector(".modal-feedback");
 var feedback_name = modal_feedback.querySelector("[name=name]");
+var form = modal_feedback.querySelector("form"); 
 var close = document.querySelector(".map-close");
 contacts_button.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	modal_feedback.classList.add("box-show");
+	modal_feedback.classList.add("modal-show");
 	feedback_name.focus();
 });
 close.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	modal_feedback.classList.remove("box-show");
+	modal_feedback.classList.remove("modal-show");
+});
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {	
+		if (modal_feedback.classList.contains("modal-show")) { 
+			evt.preventDefault();	
+			modal_feedback.classList.remove("modal-show");
+		}
+	}
 });
 
